7f6bbbc7dbc614edca64cb74b157ea0b024c1223
Efim Moskalev <efim.moskalev1911@gmail.com>
2019-05-07 23:56:43 +0400

    change to freemaker

--- src/main/java/one/two/controller/MainController.java
+++ src/main/java/one/two/controller/MainController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,10 +26,18 @@ public class MainController {
     }
     
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required=false) String filter, Model model) {
     	Iterable<Message> messages = messageRepo.findAll();
+    	
+    	if(filter != null && !filter.isEmpty()) {
+    		messages = messageRepo.findByTag(filter);
+    	} else {
+    		messages = messageRepo.findAll();
+    	}
 
-        model.put("messages", messages);
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+        
 
         return "main";
     }
@@ -43,20 +52,6 @@ public class MainController {
     	messageRepo.save(message);
     	Iterable<Message> messages = messageRepo.findAll();
     	model.put("messages", messages);
-    	return "main";
-    }
-    
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, 
-    		Map<String, Object> model) {
-    	Iterable<Message> messages;
-    	if(filter != null && !filter.isEmpty()) {
-    		messages = messageRepo.findByTag(filter);
-    	} else {
-    		messages = messageRepo.findAll();
-    	}
-    	model.put("messages", messages);
-    	
     	return "main";
     }
 }
