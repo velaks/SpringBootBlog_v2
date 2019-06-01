0e73a6861652d7c733a5ed9326728b9d246ab6bb
Efim Moskalev <efim.moskalev1911@gmail.com>
2019-06-01 23:56:34 +0400

    db migration (not finished)

--- src/main/java/one/two/domain/Message.java
+++ src/main/java/one/two/domain/Message.java
@@ -11,8 +11,8 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Message {
 	 	@Id
-	    @GeneratedValue(strategy=GenerationType.AUTO)
-	    private Integer id;
+	    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	    private Long id;
 	 	
 	 	private String text;
 
@@ -53,11 +53,11 @@ public class Message {
 			return author != null ? author.getUsername() : "<none>";
 		}
 
-		public Integer getId() {
+		public Long getId() {
 			return id;
 		}
 
-		public void setId(Integer id) {
+		public void setId(Long id) {
 			this.id = id;
 		}
 
--- src/main/java/one/two/domain/User.java
+++ src/main/java/one/two/domain/User.java
@@ -23,7 +23,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Table(name="user")
 public class User implements UserDetails {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String username;
 	private String password;
